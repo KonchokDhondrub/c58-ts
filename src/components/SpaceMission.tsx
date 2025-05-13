dec5292c19ff5b7fff8fd72fe32ae6f910401741
kd <erm.melnik@gmail.com>
2025-05-13 13:15:27 +0200

    initial commit, + Lesson & Homework 06

---
+++ src/components/SpaceMission.tsx
@@ -0,0 +1,27 @@
+import "./SpaceMission.css";
+
+export default function SpaceMission() {
+  const shipName: string = "Apollo 11";
+  const crewCount: number = 3;
+  let missionStarted: boolean = true;
+  const crewNames: string[] = ["Нил Армстронг", "Майкл Коллинз", "Базз Олдрин"];
+  const captain: [string, number] = ["Нил Армстронг", 38];
+
+  function startMission(shipName: string): string {
+    return `Миссия ${shipName} началась!`;
+  }
+
+  return (
+    <>
+      <h2>Название миссии: {shipName}</h2>
+      <p>Экипаж: {crewCount} человека</p>
+      <p>Список членов экипажа:</p>
+      <ul>
+        {crewNames.map((crew) => (
+          <li>{crew === "Нил Армстронг" ? captain.join(", ") : crew}</li>
+        ))}
+      </ul>
+      <p>{missionStarted ? startMission(shipName) : `Миссия ${shipName} ещё не началась!`}</p>
+    </>
+  );
+}
---
+++ src/hw/hw_06/Homework06.tsx
@@ -0,0 +1,9 @@
+import SpaceMission from "../../components/SpaceMission";
+
+export default function Homework06() {
+  return (
+    <div className="space-mission-container">
+      <SpaceMission />
+    </div>
+  );
+}
