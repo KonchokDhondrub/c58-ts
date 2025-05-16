e0820d3e7615b6cb12d68a1de01e69084770bdcb
kd <erm.melnik@gmail.com>
2025-05-16 14:11:04 +0200

    code update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 // import { useState } from "react";
 import "./App.css";
 
-import Def from "./hw/hw_02/Homework02";
+// import Def from "./hw/hw_02/Homework02";
 // import Def from "./hw/hw_08/Homework08";
 
 // import Def from "./lessons/lesson_06/Lesson06";
-// import Def from "./lessons/lesson_09/Lesson09";
+import Def from "./lessons/lesson_09/Lesson09";
 
 function App() {
   return (
