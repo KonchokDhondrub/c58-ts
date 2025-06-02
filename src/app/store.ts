ffd437801436b34255539c25b79519e723ac23f4
kd <erm.melnik@gmail.com>
2025-06-02 10:11:53 +0200

    axios initial commit

---
+++ src/app/store.ts
@@ -0,0 +1,14 @@
+import { type Action, configureStore, type ThunkAction } from "@reduxjs/toolkit";
+
+// * в store хранятся данные из всего react приложения
+// они изменяются с помощью функции reducer, в которую передается action
+
+export const store = configureStore({
+  reducer: {
+    // подключаем функции reducer для обработки данных из файлов slice
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
